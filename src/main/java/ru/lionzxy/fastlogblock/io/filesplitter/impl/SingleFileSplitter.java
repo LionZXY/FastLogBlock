d4254be6942875884e9929ea8380c193d347173a
lionzxy <nikita@kulikof.ru>
2018-01-30 16:15:37 +0300

    Fix single filesplitter

--- src/main/java/ru/lionzxy/fastlogblock/io/filesplitter/impl/SingleFileSplitter.java
+++ src/main/java/ru/lionzxy/fastlogblock/io/filesplitter/impl/SingleFileSplitter.java
@@ -2,6 +2,7 @@ package ru.lionzxy.fastlogblock.io.filesplitter.impl;
 
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import ru.lionzxy.fastlogblock.io.filesplitter.IFileSplitter;
 
 import java.io.File;
@@ -21,6 +22,20 @@ public class SingleFileSplitter extends IFileSplitter {
 
     @Override
     public File getFileByPosAndWorld(final BlockPos blockPos, World world) {
-        return logFile;
+        if (world == null) {
+            return new File(modFolder, "block.bytelog");
+        }
+
+        File saveFile = DimensionManager.getCurrentSaveRootDirectory();
+        if (saveFile == null) {
+            saveFile = new File("save0");
+        }
+        final File saveFolder = new File(modFolder, saveFile.getName());
+        String worldSave = world.provider.getSaveFolder();
+        if (worldSave == null) {
+            worldSave = "DIM0";
+        }
+        final File dimFolder = new File(saveFolder, new File(worldSave).getName());
+        return new File(dimFolder, "block.bytelog");
     }
 }
