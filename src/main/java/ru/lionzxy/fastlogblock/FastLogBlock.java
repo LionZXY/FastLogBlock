d32125dd5283a1d3e4923440ef464bcec52f11b9
lionzxy <nikita@kulikof.ru>
2018-01-30 16:17:05 +0300

    Update to 1.0.1

--- src/main/java/ru/lionzxy/fastlogblock/FastLogBlock.java
+++ src/main/java/ru/lionzxy/fastlogblock/FastLogBlock.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 @Mod(modid = FastLogBlock.MODID, version = FastLogBlock.VERSION, updateJSON = "https://raw.githubusercontent.com/LionZXY/FastLogBlock/master/update.json")
 public class FastLogBlock {
     public static final String MODID = "fastlogblock";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     private EventHandlingManager eventHandlingManager;
     private InfoItem infoitem;
 
